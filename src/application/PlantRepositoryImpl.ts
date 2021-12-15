8b29e5212d032e94a3b0dc2ddfe95ff7eca89a16
leojen23 <leojen23@hotmail.com>
2021-12-15 10:32:52 +0100

    cors test 2

--- src/application/PlantRepositoryImpl.ts
+++ src/application/PlantRepositoryImpl.ts
@@ -15,7 +15,7 @@ export default class PlantRepositoryImpl implements IPlantRepository {
         const requestUrl: string = requestBuilder("/api/plants")
 
         try {
-            corsHeadersSetter(axios)
+            axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
             const data: any = (await axios.get<AxiosResponse>(requestUrl)).data
             // console.log(data['hydra:member'])
             const plantsData: Plant[] = data['hydra:member']
