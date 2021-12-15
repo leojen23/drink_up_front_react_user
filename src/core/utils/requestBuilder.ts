4eab063b27a31c2b8d1f92bd4ffdd2bcbf4e5422
leojen23 <leojen23@hotmail.com>
2021-12-15 18:30:32 +0100

    final build - 3

--- src/core/utils/requestBuilder.ts
+++ src/core/utils/requestBuilder.ts
@@ -1,12 +1,12 @@
-// export const requestBuilder = (endpoint: string): string => {
-//     const protocole: string = "https://";
-//     const domain: string = "api.cda2-devops-olivier.simplon-roanne.com";
-//     const request: string = protocole + domain + endpoint;
-//     return request;
-// }
 export const requestBuilder = (endpoint: string): string => {
-    const protocole: string = "http://";
-    const domain: string = "drink-up-apiplatform.test:8080";
+    const protocole: string = "https://";
+    const domain: string = "drinkupapi.cda2-devops-olivier.simplon-roanne.com";
     const request: string = protocole + domain + endpoint;
     return request;
 }
+// export const requestBuilder = (endpoint: string): string => {
+//     const protocole: string = "http://";
+//     const domain: string = "drink-up-apiplatform.test:8080";
+//     const request: string = protocole + domain + endpoint;
+//     return request;
+// }
